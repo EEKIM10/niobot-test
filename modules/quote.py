8feb1f2c739fc975fcb376cf64e0aff2103ece37
nex <me@nexy7574.co.uk>
2023-06-15 00:42:47 +0100

    fix quote command

--- modules/quote.py
+++ modules/quote.py
@@ -4,12 +4,12 @@ import tempfile
 
 
 class QuoteModule(niobot.Module):
-    @niobot.command("quote")
+    @niobot.command("quote", aliases=['q'])
     async def quote(self, ctx: niobot.Context):
         """Generate a random quote.
         
         The source is https://inspirobot.me/"""
-        with tempfile.mkstemp(suffix=".jpg") as tmp:
+        with tempfile.TemporaryFile(suffix=".jpg") as tmp:
             async with httpx.AsyncClient() as client:
                 response = await client.get("https://inspirobot.me/api?generate=true")
                 if response.status_code != 200:
@@ -20,6 +20,7 @@ class QuoteModule(niobot.Module):
                 if response.status_code != 200:
                     await ctx.reply("Something happened and nearly succeeded!")
                     return
-                with open(tmp[1], "wb") as f:
+                with open(tmp, "wb") as f:
                     f.write(response.content)
-                await ctx.reply(url, file=tmp[1])
+                attachment = await niobot.MediaAttachment.from_file(tmp)
+                await ctx.reply(url, file=attachment)
